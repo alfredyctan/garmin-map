9f8f4c4151c13b7aec2c4010354bbc3bb67772c9
Alfred Tan <alfred.yctan@gmail.com>
2018-06-03 23:57:26 +0800

    initial commit

---
+++ make.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+if [ $# -lt 2 ]; then
+	echo "usage: $0 <env> <option>"
+	exit;
+fi
+
+ENV=$1
+OPTION=$2
+
+. $ENV
+
+#===========================
+# remove the output 
+#===========================
+mkdir -p gmap
+mkdir -p osm 
+mkdir -p srtm
+rm -rf gmap\*.*
+
+#===========================
+# download Srtm from http://osm.michis-pla.net/code/Srtm2Osm-1.12.1.0.zip and put to srtm2osm
+#
+# create topo osm source
+# download DEM (Digital Elevation Model) file from 
+# http://www.viewfinderpanoramas.org/Coverage%20map%20viewfinderpanoramas_org3.htm
+# to srtm.cache, eg. srtm.cache/SrtmCache/N09E124.hgt
+#===========================
+srtm2osm/Srtm2Osm.exe -d srtm.cache -cat 400 100 -large -corrxy 0.0005 0.0006 -bounds1 $SOUTH $WEST $NORTH $EAST -o srtm/srtm-${MAP}.osm
+
+#===========================
+echo "downloading Open Street Map Data for $MAP map"
+curl --output osm/$MAP.osm https://overpass-api.de/api/map?bbox=$WEST,$SOUTH,$EAST,$NORTH
+
+#===========================
+# download mkgmap from http://www.mkgmap.org.uk/ and put to garmin/mkgmap
+# compile osm source and topo source
+#===========================
+java -Xmx1g -jar mkgmap/${MKGMAP}/mkgmap.jar -c $OPTION srtm/srtm-$MAP.osm osm/$MAP.osm
+
+#==== rename the output and done ====
+mv gmap/gmapsupp.img gmap/gmapsupp-$MAP.img
+
+echo "gmap/gmapsupp-$MAP.img is created"
+
+
---
+++ srtm2osm/run.bat
@@ -0,0 +1,9 @@
+rem srtm2osm.exe -bounds1 57.71690 4.01368 71.19163 31.14427 -cat 400 100 -large -corrxy 0.0005 0.0006 -o no.srtm.osm
+
+rem 31.1969 -7.7165 30.9682 -8.0880
+
+rem srtm2osm.exe -bounds3 http://www.openstreetmap.org/export#map=12/31.0925/-7.9040&layers=C -cat 400 100 -large -corrxy 0.0005 0.0006 -o no.srtm.osm
+
+
+
+srtm2osm.exe -bounds1 31.1708 31.0140 -7.7423 -8.0657 -cat 400 100 -large -corrxy 0.0005 0.0006 -o no.srtm.osm
